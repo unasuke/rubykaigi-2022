1e4470d55217d6c81a133c0aee41e3f0aed30a7f
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-09-10 09:50:35 +0900

    Add benchmark case to use IO::Buffer
    ```
    $ bundle exec ruby main.rb
    Warming up --------------------------------------
        hello_world_upcase_string    697.168k i/s -    729.510k times in 1.046391s (1.43μs/i)
       hello_world_upcase_integer      1.041M i/s -      1.069M times in 1.026897s (960.65ns/i)
       489.648k i/s -    498.680k times in 1.018445s (2.04μs/i)
    Calculating -------------------------------------
        hello_world_upcase_string    725.845k i/s -      2.092M times in 2.881472s (1.38μs/i)
       hello_world_upcase_integer      1.058M i/s -      3.123M times in 2.951405s (945.09ns/i)
       477.770k i/s -      1.469M times in 3.074584s (2.09μs/i)
    
    Comparison:
       hello_world_upcase_integer :   1058097.0 i/s
        hello_world_upcase_string :    725845.4 i/s - 1.46x  slower
     hello_world_upcase_io_buffer :    477770.0 i/s - 2.21x  slower
    ```

--- benchmarks/ruby/main.rb
+++ benchmarks/ruby/main.rb
@@ -4,6 +4,8 @@ Benchmark.driver do |x|
   x.prelude <<~'RUBY'
     HELLO_WORLD_BYTESTRING = "\x68\x65\x6c\x6c\x6f\x20\x77\x6f\x72\x6c\x64\x21"
     HELLO_WORLD_BYTEARRAY = [0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]
+    HELLO_WORLD_BUFFER = IO::Buffer.for('hello world!')
+
     def hello_world_upcase_string
       upcased = ""
       HELLO_WORLD_BYTESTRING.each_byte do |byte|
@@ -26,7 +28,20 @@ Benchmark.driver do |x|
       end
       # puts upcased.map(&:chr).join # => HELLO WORLD!
     end
+
+    def hello_world_upcase_io_buffer
+      upcased_buf = IO::Buffer.new(HELLO_WORLD_BUFFER.size)
+      HELLO_WORLD_BUFFER.size.times do |i|
+        chr = HELLO_WORLD_BUFFER.get_value(:U8, i)
+        if 0x41 <= chr && chr <= 0x7a
+          upcased_buf.set_value(:U8, i, chr - 0x20)
+        else
+          upcased_buf.set_value(:U8, i, chr)
+        end
+      end
+    end
   RUBY
   x.report %{ hello_world_upcase_string }
   x.report %{ hello_world_upcase_integer }
+  x.report %{ hello_world_upcase_io_buffer }
 end
